146c7a4a349e0c9c2fcd65b6113f3aeb7e721da1
Saurabh Misra <saurabhmisra87@gmail.com>
2018-05-17 21:36:53 +0530

    Update contacts reducer to store data as lookup table rather than an array

--- src/reducers/contacts.js
+++ src/reducers/contacts.js
@@ -1,25 +1,48 @@
 import contact from './contact';
+import { combineReducers } from 'redux';
 
-const contacts = (state = [], action) => {
+const byId = (state = {}, action) => {
     switch (action.type) {
         case 'ADD_CONTACT':
-            return [
-                ...state,
-                contact(null, action)
-            ];
         case 'TOGGLE_FAVORITE':
-            return state.map((c) => contact(c, action));
+            const actionId = action.id || action.contact.id;
+            return {
+                ...state,
+                [actionId]: contact(state[actionId], action)
+            }
         default:
             return state;
     }
 };
 
+const allIds = (state = [], action) => {
+    console.log(state);
+    console.log(action);
+    switch(action.type){
+        case 'ADD_CONTACT':
+            return [
+                ...state,
+                action.contact.id
+            ];
+        default:
+            return state;
+    };
+};
+
+const contacts = combineReducers({
+    byId,
+    allIds
+});
+
 export default contacts;
 
-export const getVisibleContacts = (state, filter) => {
-    if (filter === 'favorites')
-        return state.filter((contact) => contact.isFavorite);
+const getAllContacts = (state) => state.allIds.map( id => state.byId[id] );
 
-    return state;
+export const getVisibleContacts = (state, filter) => {
+    const allContacts = getAllContacts(state);
+    if (filter === 'favorites')
+        return allContacts.filter((contact) => contact.isFavorite);
+
+    return allContacts;
 };
 
